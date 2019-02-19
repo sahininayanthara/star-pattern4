a10ec7ba8c2ed0272b9fab3b924aa995dcdd4a66
sahininayanthara <sahininayanthara95@gmail.com>
2019-02-19 22:48:56 +0530

    first commit

---
+++ Untitled16.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int x,y,z;
+	for(x=0;x<10;x++)
+	printf("* ");
+	printf("\n");
+	
+	for(y=0;y<8;y++)
+	printf("*                 *\n");
+	for(z=0;z<10;z++)
+	printf("* ");
+	
+	return 0;
+}
